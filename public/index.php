6af369b54fad4df6deace4d4180e947bd6f7bf59
Rafael Gonçalves <rafaelswitek@gmail.com>
2020-10-18 14:55:43 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,71 +1,20 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-    <title>Listagem de cliente</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-</head>
+require __DIR__ . '/../vendor/autoload.php';
 
-<body>
+use Switek\Crud\Controller\Client;
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">CRUD</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cadastrar</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+$path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/'; //Pega o caminho da url
+$routes = require __DIR__ . '/../config/routes.php'; //Carrega as rotas
 
-    <div class="container">
-        <h1 class="text-center">Listagem</h1>
+if (!array_key_exists($path, $routes)) { //Verifica se a url consta nas rotas, se não existir retorna Erro 404
+    http_response_code(404);
+    exit();
+}
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">CPF</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>John Doe</td>
-                    <td>john.doe@test.com</td>
-                    <td>000.000.000-00</td>
-                    <td>(00) 0000-0000</td>
-                    <td>
-                        <a href="#" class="btn btn-primary">Editar</a>
-                        <a href="#" class="btn btn-danger">Excluir</a>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>João Silva</td>
-                    <td>joao.silva@teste.com</td>
-                    <td>000.000.000-00</td>
-                    <td>(00) 0000-0000</td>
-                    <td>
-                        <a href="#" class="btn btn-primary">Editar</a>
-                        <a href="#" class="btn btn-danger">Excluir</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+session_start();
 
-</body>
+$method = $routes[$path]; //Pega o metodo referente a url
 
-</html>
+$client = new Client();
+$client->$method();
