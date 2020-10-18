4e67b2940208f18d365ccb50cee5b40f58ead7a8
Rafael Gonçalves <rafaelswitek@gmail.com>
2020-10-18 15:08:27 -0300

    Entity Client

---
+++ src/Entity/Client.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Switek\Crud\Entity;
+
+/**
+ * @Entity
+ */
+class Client
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+    /**
+     * @Column(type="string")
+     */
+    private $name;
+    /**
+     * @Column(type="string")
+     */
+    private $email;
+    /**
+     * @Column(type="integer")
+     */
+    private $cpf;
+    /**
+     * @Column(type="integer")
+     */
+    private $phone;
+
+
+    /* Getters e Setters*/
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): self
+    {
+        $this->cpf = $cpf;
+        return $this;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+}
